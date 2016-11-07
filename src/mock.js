6c1a6097c0d2964707013a15101d793cee2a1a1f
auridevil <onailerua@hotmail.com>
2016-11-07 15:19:42 +0100

    fixed mocking

--- src/mock.js
+++ src/mock.js
@@ -1,6 +1,6 @@
-var _ = require( "lodash" );
-var when = require( "when" );
-var mockMethods = require( "./methods" );
+var _ = require("lodash");
+var when = require("when");
+var mockMethods = require("./methods");
 
 /* Example mock object structure
 
@@ -15,140 +15,163 @@ var mockMethods = require( "./methods" );
 
 */
 
-function executeSqlFn( sql, fallbackFn ) {
-	return function( options ) {
-		var stepName = this.state;
-		var mock = sql.resolveMock( stepName, options );
+function executeSqlFn(sql, fallbackFn) {
+    return function(step,stepName,options) {
+        var mock = sql.resolveMock(stepName, options);
 
-		if ( !_.isUndefined( mock ) ) {
-			return when.promise( function( resolve, reject ) {
-				var results = mock.mockResults.call( this, stepName, options );
-				var timeout = mock.waitTime || 0;
+        if (!_.isUndefined(mock)) {
+            return when.promise(function(resolve, reject) {
+                var results = mock.mockResults.call(this, stepName, options);
+                var timeout = mock.waitTime || 0;
 
-				setTimeout( function() {
-					if ( mock.isError ) {
-						reject( results );
-					} else {
-						resolve( results );
-					}
-				}, timeout );
+                setTimeout(function() {
+                    if (mock.isError) {
+                        reject(results);
+                    } else {
+                        resolve(results);
+                    }
+                }, timeout);
 
-			}.bind( this ) );
-		}
+            }.bind(this));
+        }
 
-		return fallbackFn.call( this, options );
-	};
+        return fallbackFn.call(this, options);
+    };
 }
 
 module.exports = {
 
-	setup: function( sql, options ) {
-		options = options || {};
+    setup: function(sql, options) {
+        options = options || {};
 
-		sql.mockCache = {};
-		sql.mockConfig = {};
+        sql.mockCache = {};
+        sql.mockConfig = {};
 
-		_.extend( sql, mockMethods );
+        _.extend(sql, mockMethods);
 
-		var defaultConfig = {
-			ignoreFailedConnections: true
-		};
+        var defaultConfig = {
+            ignoreFailedConnections: true
+        };
 
-		sql.setMockConfig( _.merge( defaultConfig, options ) );
+        sql.setMockConfig(_.merge(defaultConfig, options));
 
-		this.patchSeriate( sql );
+        this.patchSeriate(sql);
 
-		this.patchSqlContext( sql );
+        this.patchSqlContext(sql);
 
-		this.patchTransactionContext( sql );
+        this.patchTransactionContext(sql);
 
-	},
+    },
 
-	patchSeriate: function( sql ) {
-		var _fromFile = sql.fromFile;
+    patchSeriate: function(sql) {
+        var _fromFile = sql.fromFile;
 
-		sql.fromFile = function( p ) {
-			p = this._getFilePath( p );
+        sql.fromFile = function(p) {
+            p = this._getFilePath(p);
 
-			var mockExists = !_.isUndefined( this.getMock( p, { cacheKey: "file" } ) );
+            var mockExists = !_.isUndefined(this.getMock(p, {
+                    cacheKey: "file"
+                }));
 
-			if ( mockExists ) {
-				return "file://" + p;
-			} else {
-				return _fromFile.call( this, p );
-			}
+            if (mockExists) {
+                return "file://" + p;
+            } else {
+                return _fromFile.call(this, p);
+            }
 
-		};
-	},
+        };
+    },
 
-	patchSqlContext: function( sql ) {
-		var origContext = sql.SqlContext;
-		var _executeSql = origContext.prototype.executeSql;
-		var _errorState = origContext.prototype.states.connecting.error;
+    patchSqlContext: function(sql) {
+        var origContext = sql.SqlContext;
+        var _executeSql = origContext.prototype.executeSql;
+        var _errorState = origContext.prototype.states.connecting.error;
 
-		sql.SqlContext = origContext.extend( {
-			executeSql: executeSqlFn( sql, _executeSql ),
-			states: {
-				connecting: {
-					error: function( err ) {
-						if ( sql.getMockConfig( "ignoreFailedConnections" ) ) {
-							return this.states.connecting.success.call( this );
-						}
-						return _errorState.call( this, err );
-					}
+		// var md5sum = require('crypto').createHash('md5');
+		// md5sum.update(require('util').inspect(sql.SqlContext,showHidden=true, depth=5));
+		// console.log('PRE ',md5sum.digest('hex'));
+
+		sql.SqlContext = origContext.extend({
+            executeSql: executeSqlFn(sql, _executeSql),
+            states: {
+                connecting: {
+                    error: function(err) {
+                        if (sql.getMockConfig("ignoreFailedConnections")) {
+                            return this.states.connecting.success.call(this);
+                        }
+                        return _errorState.call(this, err);
+                    }
+                }
+            }
+        });
+
+		if(!sql.SqlContext.prototype.executeSql.isSinonProxy) {
+			require('sinon').stub(sql.SqlContext.prototype, 'executeSql', executeSqlFn(sql, _executeSql));
+		}
+		if(!sql.SqlContext.prototype.states.connecting.error.isSinonProxy) {
+			require('sinon').stub(sql.SqlContext.prototype.states.connecting, 'error', function (err) {
+				if (sql.getMockConfig("ignoreFailedConnections")) {
+					return this.states.connecting.success.call(this);
 				}
-			}
-		} );
-	},
+				return _errorState.call(this, err);
+			});
+		}
 
-	patchTransactionContext: function( sql ) {
-		var origContext = sql.TransactionContext;
-		var _executeSql = origContext.prototype.executeSql;
-		var _errorState = origContext.prototype.states.connecting.error;
+		sql.SqlContext['mockedSym'] = Symbol('mock');
+    //
+		// var md5sum2 = require('crypto').createHash('md5');
+		// md5sum2.update(require('util').inspect(sql.SqlContext,showHidden=true, depth=5));
+		// console.log('POST',md5sum2.digest('hex'));
+    },
 
-		sql.TransactionContext = origContext.extend( {
-			executeSql: executeSqlFn( sql, _executeSql ),
-			states: {
-				connecting: {
-					error: function( err ) {
-						if ( sql.getMockConfig( "ignoreFailedConnections" ) ) {
-							var success = this.states.connecting.success;
-							if ( _.isString( success ) ) {
-								return this.transition( success );
-							} else {
-								return success.call( this );
-							}
-						}
-						return _errorState.call( this, err );
-					}
-				},
-				startingTransaction: {
-					_onEnter: function() {
-						return this.states.startingTransaction.success.call( this );
-					}
-				},
-				done: {
-					_onEnter: function() {
-						var self = this;
-						self.emit( "end", {
-							sets: self.results,
-							transaction: {
-								commit: function() {
-									return when.promise( function( resolve ) {
-										resolve();
-									} );
-								},
-								rollback: function() {
-									return when.promise( function( resolve ) {
-										resolve();
-									} );
-								}
-							}
-						} );
-					}
-				}
-			}
-		} );
-	}
+    patchTransactionContext: function(sql) {
+        var origContext = sql.TransactionContext;
+        var _executeSql = origContext.prototype.executeSql;
+        var _errorState = origContext.prototype.states.connecting.error;
+
+        sql.TransactionContext = origContext.extend({
+            executeSql: executeSqlFn(sql, _executeSql),
+            states: {
+                connecting: {
+                    error: function(err) {
+                        if (sql.getMockConfig("ignoreFailedConnections")) {
+                            var success = this.states.connecting.success;
+                            if (_.isString(success)) {
+                                return this.transition(success);
+                            } else {
+                                return success.call(this);
+                            }
+                        }
+                        return _errorState.call(this, err);
+                    }
+                },
+                startingTransaction: {
+                    _onEnter: function() {
+                        return this.states.startingTransaction.success.call(this);
+                    }
+                },
+                done: {
+                    _onEnter: function() {
+                        var self = this;
+                        self.emit("end", {
+                            sets: self.results,
+                            transaction: {
+                                commit: function() {
+                                    return when.promise(function(resolve) {
+                                        resolve();
+                                    });
+                                },
+                                rollback: function() {
+                                    return when.promise(function(resolve) {
+                                        resolve();
+                                    });
+                                }
+                            }
+                        });
+                    }
+                }
+            }
+        });
+    }
 
 };
